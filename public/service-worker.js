b9cc97fa8da92553031b134aab0336954ad287f2
Yoga Cats <esherrow77@gmail.com>
2021-08-20 17:08:31 -0400

    added service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,18 @@
-const FILES_TO_CACHE=[
-    "../models/transaction.js",
-    "./css/style.css",
-    "./js/idb.js",
-    "./index.js",
-    "./index.html",
-    "./mainfest.json",
-    "../routes/api.js"
-];
-
 const APP_PREFIX = 'BudgetTracker';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE=[
+    "../models/transaction.js",
+    "../public/css/style.css",
+    "../public/js/idb.js",
+    "../public/js/index.js",
+    "../public/index.html",
+    "../public/mainfest.json",
+    "../routes/api.js"
+];
+
+
 
 self.addEventListener('install', function (evt){
     evt.waitUntil(
